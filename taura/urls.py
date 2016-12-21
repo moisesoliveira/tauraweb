1ed6e3ce2b6d47566cd773eb7d0c36f852d5b894
root <adrianomjcp@gmail.com>
2016-12-21 21:31:05 -0200

    adicionei as bibliotecas locais do bootstrap

--- taura/urls.py
+++ taura/urls.py
@@ -25,7 +25,8 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     # Pagina inicial usando TemplateView (chama o HTML diretamente sem precisar de funcao ou classe na views.py)
-#    url(r'^$', TemplateView.as_view(template_name="index.html")),
+#   url(r'^$', TemplateView.as_view(template_name="index.html")),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^CriaUsuario/', include('membros.urls')),
     url(r'^membros/', include('membros.urls')),
