f3bf82baa09793903f792acbfef28b5b11c2f3e3
Moises <noodle182@gmail.com>
2016-12-21 02:15:33 -0200

    com home funcionando com blog

--- membros/urls.py
+++ membros/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^(?P<id>[0-9]+)/$', views.membro, name='membro'),
     url(r'^(?P<id>[0-9]+)/excluir/$', views.excluir, name='excluir_membro'),
     url(r'^criar/$', CriaMembro.as_view(), name='criar_membro'),
-#    url(r'^atualizar/(?P<pk>\d+)/$', AtualizarAutor.as_view(), name='atualizar_autor'),
+##    url(r'^atualizar/(?P<pk>\d+)/$', AtualizarAutor.as_view(), name='atualizar_autor'),
     url(r'^login/$', logar, name='login'),
     url(r'^logout/$', sair, name='logout'),
 
